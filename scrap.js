b4003c672f0c55f6b46c22d3d23a679cc7f7cbe1
guidecayeux <gui.decayeux@gmail.com>
2023-02-21 23:28:39 +0100

    script & result

---
+++ scrap.js
@@ -0,0 +1,34 @@
+const result = {};
+
+
+let domParser = new DOMParser();
+let parsedData;
+
+function getPage(categorie, page) {
+    fetch(`https://steamcommunity.com/workshop/browse/?appid=252490&searchtext=&childpublishedfileid=0&browsesort=accepted&section=mtxitems&requiredtags%5B%5D=${categorie}&created_date_range_filter_start=0&created_date_range_filter_end=0&updated_date_range_filter_start=0&updated_date_range_filter_end=0&p=${page}`)
+        .then((brutResponse) => {
+            brutResponse.text().then((resultHtml) => {
+                parsedData = domParser.parseFromString(resultHtml, 'text/html');
+                parsedData.querySelectorAll('[data-publishedfileid]').forEach((item) => {
+                    result[categorie].push(item.getAttribute('data-publishedfileid'));
+                })
+                if (document.querySelectorAll('.pagelink').length > 0 && page <= document.querySelectorAll('.pagelink').length) {
+                    getPage(categorie, page + 1)
+                }
+            });
+
+        })
+}
+fetch(`https://steamcommunity.com/workshop/browse/?appid=252490&browsesort=accepted&section=mtxitems`)
+    .then((pageGlobal) => {
+        pageGlobal.text().then((pageGlobalHtml) => {
+            parsedData = domParser.parseFromString(pageGlobalHtml, 'text/html');
+            parsedData.querySelectorAll('.filterOption > label').forEach((item) => {
+                result[item.innerText.trim()] = [];
+                getPage(item.innerText.trim(), 1);
+            });
+        });
+    })
+
+console.log(result);
+
